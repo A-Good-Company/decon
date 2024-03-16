8409a3e3af52c7f5f9b39d851b5bb4e85cd166ab
Mayank Kumar <mayank.kr@pm.me>
2024-03-15 21:31:36 -0400

    Audio List parsed at button click

--- frontdecon/src/components/MediaRecorder.vue
+++ frontdecon/src/components/MediaRecorder.vue
@@ -1,12 +1,12 @@
 <template>
-    <div
-        class="bg-gray-200 text-gray-900 text-l rounded-md min-h-[32px] px-3 py-0.5 font-semibold hover:bg-gray-400 m-1 w-2/4  items-center ">
-        <button type="new" @mousedown="startRecording()" @mouseup="stopRecording()">
+    <div class="bg-gray-200 text-gray-900 text-l rounded-md min-h-[32px] px-3 py-0.5 font-semibold hover:bg-gray-400 m-1 w-2/4 items-center ">
+        <button type="new" @mousedown="initiateRecording()" @mouseup="stopRecording()">
             {{ isRecording ? `Recording audio, release to stop` : 'Push to record Audio' }}
         </button>
         <select v-model="selectedDeviceId" class="max-w-full">
-            <option v-for="device in devices" :key="device.deviceId" :value="device.deviceId">{{ device.label ||
-            device.deviceId }}</option>
+            <option v-for="device in devices" :key="device.deviceId" :value="device.deviceId">
+                {{ device.label || device.deviceId }}
+            </option>
         </select>
     </div>
 </template>
@@ -14,25 +14,44 @@
 
 <script>
 export default {
-    components: {
-    },
     data() {
         return {
             mediaRecorder: null,
             chunks: [],
             audio: null,
             isRecording: false,
-            devices: []
+            devices: [],
+            selectedDeviceId: null
 
         }
     },
     methods: {
+        async initiateRecording() {
+            if(this.devices.length === 0) {
+                await this.getDevices();
+            }
+            await this.startRecording();
+        },
+
+        async getDevices() {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+            this.devices = (await navigator.mediaDevices.enumerateDevices())
+                .filter(device => device.kind === 'audioinput');
+            this.selectedDeviceId = this.devices[0]?.deviceId;
+
+            // stop the streams
+            if (stream) { stream.getTracks().forEach(track => track.stop()) }
+        },
+
         async startRecording() {
+            if (!this.selectedDeviceId) return;
+
             const stream = await navigator.mediaDevices.getUserMedia({ audio: { deviceId: this.selectedDeviceId } });
 
             this.mediaRecorder = new MediaRecorder(stream);
             this.mediaRecorder.start();
-            this.isRecording = true
+            this.isRecording = true;
 
             this.mediaRecorder.ondataavailable = (e) => {
                 this.chunks.push(e.data);
@@ -44,50 +63,24 @@ export default {
             this.mediaRecorder.stop();
 
             this.mediaRecorder.onstop = () => {
-                this.audio = new Blob(this.chunks, { 'type': 'audio/webm; codecs=opus' });
-                const timestamp = new Date().getTime();
-
-                // We're done, now we can send it to OpenAI
-                // convert blobData to a file before calling OpenAi.detectText
-                const file = new File([this.audio], `recording_${timestamp}.webm`, {
-                    type: 'audio/webm; codecs=opus',
-                    lastModified: Date.now()
-                });
-                this.$emit('audioFile', file)
-                this.chunks = [];
-                this.isRecording = false;
+                this.finalizeRecording();
             };
         },
-    },
-    mounted() {
-        if (!navigator.mediaDevices?.enumerateDevices) {
-            console.log("enumerateDevices() not supported.");
-        } else {
-            // List cameras and microphones.
-            navigator.mediaDevices.getUserMedia({ audio: true})
-                .then((stream) => {
 
-                    navigator.mediaDevices.enumerateDevices()
-                        .then((devices) => {
-                            this.devices = devices.filter(device => device.kind === 'audioinput');
-                            this.selectedDeviceId = this.devices[0]?.deviceId;
+        finalizeRecording() {
+            this.audio = new Blob(this.chunks, { 'type': 'audio/webm; codecs=opus' });
+            const timestamp = new Date().getTime();
 
-                            // stop the streams
-                            if (stream) { stream.getTracks().forEach(track => track.stop()) }
-                        })
-                        .catch((err) => {
-                            console.error(`${err.name}: ${err.message}`);
-                        });
+            const file = new File([this.audio], `recording_${timestamp}.webm`, {
+                type: 'audio/webm; codecs=opus',
+                lastModified: Date.now()
+            });
 
-                })
-                .catch((err) => {
-                    console.error(`${err.name}: ${err.message}`);
-                })
+            this.chunks = [];
+            this.isRecording = false;
 
-        }
-
+            this.$emit('audioFile', file);
+        },
     }
-
-
 }
 </script>
